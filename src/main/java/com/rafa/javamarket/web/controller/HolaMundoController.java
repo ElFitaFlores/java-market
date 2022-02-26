c1a91d7fb6ed99d958bab3f1b526fc318a29cef0
Rafael Flores <fita500@gmail.com>
2022-02-25 19:19:38 -0600

    Move HolaMundoController to controller package

--- src/main/java/com/rafa/javamarket/web/controller/HolaMundoController.java
+++ src/main/java/com/rafa/javamarket/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.rafa.javamarket;
+package com.rafa.javamarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
