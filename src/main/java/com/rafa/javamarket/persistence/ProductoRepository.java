c5cc100fc8d03e37f24973cabf2cb1dfaaa7a41a
Rafael Flores <fita500@gmail.com>
2022-02-27 18:26:36 -0600

    Add save and delete methods to ProductoRepository

--- src/main/java/com/rafa/javamarket/persistence/ProductoRepository.java
+++ src/main/java/com/rafa/javamarket/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.rafa.javamarket.persistence;
 
 import com.rafa.javamarket.persistence.crud.ProductoCrudRepository;
 import com.rafa.javamarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -17,7 +19,19 @@ public class ProductoRepository {
         return productoCrudRepository.findByIdCategoria(idCategoria);
     }
 
-    public Optional<List<Producto>> getEscazos(int cantidad, boolean estado) {
+    public Optional<List<Producto>> getEscasos(int cantidad, boolean estado) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, estado);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
