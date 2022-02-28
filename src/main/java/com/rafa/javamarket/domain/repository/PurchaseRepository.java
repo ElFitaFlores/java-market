998ac8ae740cc7a5c42a0b67d03af83514f29e65
Rafael Flores <fita500@gmail.com>
2022-02-27 21:07:34 -0600

    Add purchase domain

---
+++ src/main/java/com/rafa/javamarket/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.rafa.javamarket.domain.repository;
+
+import com.rafa.javamarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
